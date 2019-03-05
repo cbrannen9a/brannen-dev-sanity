6ddbe29d6612cafbefe5e750a5418cebeb721c56
Chris Brannen <cbrannen@outlook.com>
2019-03-05 15:26:47 +0000

    Setup

---
+++ schemas/project.js
@@ -0,0 +1,60 @@
+export default {
+	name: 'project',
+	title: 'Project',
+	type: 'document',
+	fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string'
+		},
+		{
+			name: 'slug',
+			title: 'Slug',
+			type: 'slug',
+			options: {
+				source: 'title',
+				maxLength: 96
+			}
+		},
+		{
+			name: 'author',
+			title: 'Author',
+			type: 'reference',
+			to: { type: 'author' }
+		},
+		{
+			name: 'mainImage',
+			title: 'Main image',
+			type: 'image',
+			options: {
+				hotspot: true
+			}
+		},
+		{
+			name: 'skills',
+			title: 'Skills',
+			type: 'array',
+			of: [{ type: 'reference', to: { type: 'skills' } }]
+		},
+		{
+			name: 'description',
+			title: 'Description',
+			type: 'blockContent'
+		},
+		{
+			name: 'body',
+			title: 'Body',
+			type: 'blockContent'
+		}
+	],
+
+	preview: {
+		select: {
+			title: 'title',
+			description: 'description',
+			skills: 'skills',
+			media: 'mainImage'
+		}
+	}
+}
---
+++ schemas/skills.js
@@ -0,0 +1,17 @@
+export default {
+	name: 'skills',
+	title: 'Skills',
+	type: 'document',
+	fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string'
+		},
+		{
+			name: 'description',
+			title: 'Description',
+			type: 'blockContent'
+		}
+	]
+}
