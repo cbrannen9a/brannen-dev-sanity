0681aaa95b61582c778e461964b1b0be3afd8033
Chris Brannen <cbrannen@outlook.com>
2019-03-18 15:06:18 +0000

    add description to posts and prettier formatting

--- schemas/project.js
+++ schemas/project.js
@@ -1,61 +1,61 @@
 export default {
-	name: 'project',
-	title: 'Project',
-	type: 'document',
-	fields: [
-		{
-			name: 'title',
-			title: 'Title',
-			type: 'string'
-		},
-		{
-			name: 'slug',
-			title: 'Slug',
-			type: 'slug',
-			options: {
-				source: 'title',
-				maxLength: 96
-			}
-		},
-		{
-			name: 'author',
-			title: 'Author',
-			type: 'reference',
-			to: { type: 'author' }
-		},
-		{
-			name: 'mainImage',
-			title: 'Main image',
-			type: 'image',
-			options: {
-				hotspot: true
-			}
-		},
-		{
-			name: 'skills',
-			title: 'Skills',
-			type: 'array',
-			of: [{ type: 'reference', to: { type: 'skills' } }]
-		},
-		{
-			name: 'description',
-			title: 'Description',
-			type: 'text'
-		},
-		{
-			name: 'body',
-			title: 'Body',
-			type: 'blockContent'
-		},
-		{
-			name: 'url',
-			title: 'Url',
-			type: 'url'
-		},
-		{
-			name: 'publishedAt',
-			title: 'Published at',
-			type: 'datetime'
-		},
-	],
-}
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96
+            }
+        },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: { type: 'author' }
+        },
+        {
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: 'skills',
+            title: 'Skills',
+            type: 'array',
+            of: [{ type: 'reference', to: { type: 'skills' } }]
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text'
+        },
+        {
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent'
+        },
+        {
+            name: 'url',
+            title: 'Url',
+            type: 'url'
+        },
+        {
+            name: 'publishedAt',
+            title: 'Published at',
+            type: 'datetime'
+        }
+    ]
+};
--- schemas/skills.js
+++ schemas/skills.js
@@ -1,57 +1,49 @@
 export default {
-	name: 'skills',
-	title: 'Skills',
-	type: 'document',
-	fields: [
-		{
-			name: 'title',
-			title: 'Title',
-			type: 'string'
-		},
-		{
-			name: 'description',
-			title: 'Description',
-			type: 'blockContent'
-		},
-		{
-			name: 'group',
-			title: 'Group',
-			type: 'number'
-		},
-		{
-			name: 'subGroup',
-			title: 'Sub Group',
-			type: 'number'
-
-		},
-		{
-			name: 'order',
-			title: 'Order',
-			type: 'number'
-
-		},
-	],
-	orderings: [
-		{
-			title: 'Group, Order',
-			name: 'groupAsc',
-			by: [
-				{ field: 'group', direction: 'asc' }
-			]
-		},
-		{
-			title: 'SubGroup, Order',
-			name: 'subGroupAsc',
-			by: [
-				{ field: 'subGroup', direction: 'asc' }
-			]
-		},
-		{
-			title: 'Order, Order',
-			name: 'orderAsc',
-			by: [
-				{ field: 'order', direction: 'asc' }
-			]
-		}
-	]
-}
+    name: 'skills',
+    title: 'Skills',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'blockContent'
+        },
+        {
+            name: 'group',
+            title: 'Group',
+            type: 'number'
+        },
+        {
+            name: 'subGroup',
+            title: 'Sub Group',
+            type: 'number'
+        },
+        {
+            name: 'order',
+            title: 'Order',
+            type: 'number'
+        }
+    ],
+    orderings: [
+        {
+            title: 'Group, Order',
+            name: 'groupAsc',
+            by: [{ field: 'group', direction: 'asc' }]
+        },
+        {
+            title: 'SubGroup, Order',
+            name: 'subGroupAsc',
+            by: [{ field: 'subGroup', direction: 'asc' }]
+        },
+        {
+            title: 'Order, Order',
+            name: 'orderAsc',
+            by: [{ field: 'order', direction: 'asc' }]
+        }
+    ]
+};
